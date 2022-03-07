d0060b7e4dca802bd292310b1a596779f3c4f780
sgrigerc <sgrigerc@gmail.com>
2022-03-07 19:59:24 +0300

    yes

--- dj1/settings.py
+++ dj1/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +142,5 @@ LOGIN_URL = 'user'
 
 MEDIA_URL = '/pictures/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+
+django_heroku.settings(locals())
