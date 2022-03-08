261991cb296493ccee8fa85101664103f8613f98
sgrigerc <sgrigerc@gmail.com>
2022-03-08 20:26:33 +0300

    page contacti

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ShowNewsView.as_view(), name = 'home'),
+    path('', views.ShowNewsView.as_view(), name = 'blog-home'),
     path('user/<str:username>', views.UserAllNewsView.as_view(), name = 'user-news'),
     path('news/<int:pk>', views.NewsDetailView.as_view(), name = 'news-detail'),
     path('news/<int:pk>/update', views.UpdateNewsView.as_view(), name = 'news-update'),
